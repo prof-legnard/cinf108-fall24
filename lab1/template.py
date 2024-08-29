87007ebce52565fcb472b35c143ee46b847c2505
prof-legnard <tlegnard@albany.edu>
2024-08-29 10:16:07 -0400

    Update template.py

--- lab1/template.py
+++ lab1/template.py
@@ -25,6 +25,24 @@ print("")
 print("")
 print("")
 
+"""
+4. How many hours are in the month of September? Write some python code to show your work and calculate the value. 
+Print the solution with the following statement, where `{num_days}` is the integer value of the number of days in September:
+
+
+There are {num_days} days in September
+Provide your code below.
+"""
+
+"""
+5. What  is 101 divided by 4, rounded down to the nearest integer? What is the remainder (modulo)?
+6. Show your calculation in python and print and label both values in one statement.
+"""
+
+
+
+
+
 """BONUS (2 points): Print your name in block letters. each letter 
 should be at least 4 lines high:
 HINT: What is ASCII art?
