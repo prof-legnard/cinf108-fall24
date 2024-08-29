0377e984a30c0fa34c84692807337fc8c4d57da1
prof-legnard <tlegnard@albany.edu>
2024-08-29 09:48:05 -0400

    Create template.py

---
+++ lab1/template.py
@@ -0,0 +1,41 @@
+"""
+#1
+Print the string "Hi my name is {your name}, this is my first print statement" in three different ways. Refer to the lecture notes on different ways of printing strings.
+Add your code below.
+"""
+
+name = ""
+
+
+"""
+# 2
+Copy your code from #1, edit the strings so that they match the format "Hi my name is {name}, this is print statement # {n}", where n is an integer value.
+Make sure that the number you are printing is of type int Add an extra line and 4 spaces to each print statement so the output looks like this:
+
+"Hi my name is Tom, this is print statement # 1."
+
+    "Hi my name is Tom, this is print statement # 2."
+
+        "Hi my name is Tom, this is print statement # 3."
+Add your code below.
+"""
+name = ""
+
+print("")
+print("")
+print("")
+
+"""BONUS (2 points): Print your name in block letters. each letter 
+should be at least 4 lines high:
+HINT: What is ASCII art?
+
+Example Output:
+ ____             __    _                                   _ 
+|  _ \ _ __ ___  / _|  | |    ___  __ _ _ __   __ _ _ __ __| |
+| |_) | '__/ _ \| |_   | |   / _ \/ _` | '_ \ / _` | '__/ _` |
+|  __/| | | (_) |  _|  | |__|  __/ (_| | | | | (_| | | | (_| |
+|_|   |_|  \___/|_|(_) |_____\___|\__, |_| |_|\__,_|_|  \__,_|
+                                  |___/                       
+"""
+
+
